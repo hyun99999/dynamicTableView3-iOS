45aad8547f4ed0b7221a630cc2ecb7690c6e6790
hyungyu Kim <ypsophila97@icloud.com>
2021-02-17 21:07:28 +0900

    Add MyHeartBtn.swift for cutomizing toggle btn

---
+++ dynamic_table_view/MyHeartBtn.swift
@@ -0,0 +1,17 @@
+//
+//  MyHeartButton.swift
+//  dynamic_table_view
+//
+//  Created by kimhyungyu on 2021/02/17.
+//  Copyright © 2021 Tuentuenna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MyHeartBtn: UIButton {
+    override class func awakeFromNib() {
+        //nib 파일에 대한 라이프사이클
+        super.awakeFromNib()
+        print("MyHeartBtn - awakeFromNib() called")
+    }
+}
